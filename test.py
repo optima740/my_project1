e6d1fc4a3a0f1b26b352795785ea7161bf716c82
Andrey Melnikov <k253vo@mail.ru>
2019-05-12 15:05:19 +0400

    face2

---
+++ test.py
@@ -0,0 +1,14 @@
+operator = input("Znak ")
+a = input("A= ")
+b = input("B= ")
+
+if operator == "+":
+    c = a + b
+    print("otvet= ", c)
+elif operator == "-":
+    c = a + b
+    print("otvet= ", c)
+else:
+    print("otvet HUY s gory")
+
+input()
