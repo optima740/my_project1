dd8555849b806f6498bd816f5a6bb3713d4ea77d
Andrey Melnikov <k253vo@mail.ru>
2019-05-20 13:50:59 +0400

    test_for_list

---
+++ My_Tests.py
@@ -0,0 +1,28 @@
+from My_List import*
+
+
+
+s_List = LinkedList()
+
+s_List.add_in_tail(Node(101))
+s_List.add_in_tail(Node(105))
+s_List.add_in_tail(Node(103))
+s_List.add_in_tail(Node(105))
+s_List.add_in_tail(Node(105))
+
+s_List.print_all_nodes()
+
+print(s_List.find_all(105))
+
+
+
+
+
+
+
+
+
+
+
+
+
