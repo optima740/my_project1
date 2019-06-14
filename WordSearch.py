da8f38884c265441790f213bb486d6463b8dfedf
Andrey Melnikov <k253vo@mail.ru>
2019-06-14 16:11:06 +0400

    word-14-06-19

--- WordSearch.py
+++ WordSearch.py
@@ -1,17 +1,27 @@
 def WordSearch(len,s):
     lenM = int(round((lenstr(s)/len)))
-    masstr = [[' ' for j in range(lenM)] for i in range(len)]
+    masstr = [[' ' for j in range(len)] for i in range(lenM)]
     iter=0
 
-    for i in range (len):
-        for j in range (lenM):
-            if iter!= lenstr(s):
-
+    for i in range (lenM):
+        for j in range (len):
+            if (iter!= lenstr(s)) :
                 masstr[i][j] = s[iter]
                 iter += 1
+
             else: break
 
     print(masstr, s, end='',)
+
+def perenos(masstr, s, len, lenM):
+    iter = 0
+    for i in range (lenM):
+        for j in range (len):
+            if (j == (len-1)) and masstr[i][j] != ' ' and masstr[i+1][0] != ' ':
+                masstr[i][j] = s[iter]
+                iter += 1
+
+            else: break
 
 
 def lenstr(s):
