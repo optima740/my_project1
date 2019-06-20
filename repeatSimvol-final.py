e34124efa2072870af8c0117e753d90d97d1313f
Andrey Melnikov <k253vo@mail.ru>
2019-06-20 09:18:29 +0400

    repeatSimvol

---
+++ repeatSimvol-final.py
@@ -0,0 +1,39 @@
+def HowManyTimes(s, s_generic):
+    if len(s)>len(s_generic) or (len(s)==0 or len(s_generic)==0):
+        return None
+
+    s_genlist = []
+    for i in range(len(s_generic)):
+        s_genlist.append(s_generic[i])
+    keylist = []
+    for k in range(len(s)):
+        keylist.append(s[k])
+    i=0
+    k=0
+    count_s =0
+    count_key =0
+    count_list = []
+
+    while (k < len(keylist)):
+        for i in range(len(s_genlist)):
+
+            if (s_genlist[i] == keylist[k]):
+                count_s +=1
+        if count_s != 0:
+            count_list.append(count_s)
+            count_s = 0
+
+        k+=1
+
+    i=0
+    f =0
+    rez = 1
+    for i in range (len(count_list)):
+        rez = (rez*count_list[i])
+        f = 1
+
+    if f != 0:
+        return rez
+    else:
+        return
+
