70ec34672f8625f88117997ebc1a439fdae3a086
Andrey Melnikov <k253vo@mail.ru>
2019-10-09 15:28:37 +0400

    stack_bonus

--- level_1/My_stack.py
+++ level_1/My_stack.py
@@ -7,7 +7,6 @@ class Stack:
 
     def pop(self):
         if self.size() != 0:
-
             return self.stack.pop(0)
         else:
             return None # если стек пустой
@@ -20,7 +19,6 @@ class Stack:
 
     def peek(self):
         if self.size() != 0:
-
             return self.stack[0]
         else:
             return None  # если стек пустой
@@ -29,63 +27,51 @@ def skobki(string):
     st1 = Stack()
     count_open = 0
     count_close = 0
-    for i in range(len(string)-1,-1,-1):
-        st1.push(string[i])
-    if st1.peek() == ')':
-        return False
-    else:
-        st1.pop()
-        count_open+=1
-        while st1.size()>0:
-            itm = st1.pop()
-            if itm == '(':
-                count_open +=1
-            elif itm == ')':
-                count_close += 1
-            if st1.size()==0 and itm == '(':
-                return False
-        if count_open == count_close:
-            return True
-        else:
+    if len(string) != 0:
+        for i in range(len(string)-1,-1,-1):
+            st1.push(string[i])
+        if st1.peek() == ')':
             return False
+        else:
+            st1.pop()
+            count_open += 1
+            while st1.size() > 0:
+                itm = st1.pop()
+                if itm == '(':
+                    count_open += 1
+                elif itm == ')':
+                    count_close += 1
+                if st1.size()==0 and itm == '(':
+                    return False
+            if count_open == count_close:
+                return True
+            else:
+                return False
+    else:
+        return
 
 def StackToStack(string):
     st1 = Stack()
     st2 = Stack()
-    for i in range(len(string)-1,-1,-1):
-        if string[i] != ' ':
-            st1.push(string[i])
-    while st1.size() > 0:
-        tmp = st1.pop()
-
-        if (tmp != '+') and (tmp != '*') and (tmp != '='):
-            st2.push(int(tmp))
-        elif tmp == '+' and st2.size != 0:
-            rez  = st2.pop()+ st2.pop()
-            st2.push(rez)
-        elif tmp == '*' and st2.size != 0:
-
-            rez  = st2.pop()*st2.pop()
-            st2.push(rez)
-        elif tmp == '=':
-            return st2.pop()
-
-
-
+    if len(string) != 0:
+        for i in range(len(string)-1,-1,-1):
+            if string[i] != ' ':
+                st1.push(string[i])
+        while st1.size() > 0:
+            tmp = st1.pop()
+            if (tmp != '+') and (tmp != '*') and (tmp != '='):
+                st2.push(int(tmp))
+            elif tmp == '+' and st2.size != 0:
+                rez = st2.pop() + st2.pop()
+                st2.push(rez)
+            elif tmp == '*' and st2.size != 0:
+                rez = st2.pop()*st2.pop()
+                st2.push(rez)
+            elif tmp == '=':
+                return st2.pop()
+    else:
+        return
 
 
 
-"""
-string1 = '82+5*9+='
-s = StackToStack(string1)
-print(s)
 
-
-st = Stack()
-
-st.push(0)
-
-#print(st.peek())
-
-print(st.peek())
-"""
